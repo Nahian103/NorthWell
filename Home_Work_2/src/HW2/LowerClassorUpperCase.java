9642a1f7db42081222bcfc0d6297c15b00715f57
Nahian103 <106032958+Nahian103@users.noreply.github.com>
2022-06-17 20:29:41 -0400

    NahianHW2

---
+++ Home_Work_2/src/HW2/LowerClassorUpperCase.java
@@ -0,0 +1,16 @@
+package HW2;
+
+public class LowerClassorUpperCase {
+
+	public static void main(String[] args) {
+		char ch = 'e';
+		if(ch>='A'&& ch<='Z') {
+			System.out.println("Upper Case");
+		}
+		else {
+			System.out.println("Lower Case");
+		}
+
+	}
+
+}
